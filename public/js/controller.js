9c3f4b2967215759753f8abbc922bcba208ad4a5
torzchi <117668600+torzchi@users.noreply.github.com>
2024-04-24 23:34:14 +0300

    playback limit and strikes

--- public/js/controller.js
+++ public/js/controller.js
@@ -3,8 +3,12 @@
 let currentSong;
 let guessInput; // Declare guessInput variable outside the function
 let songNames = []; // Store song names fetched from songs.json
+let playbackTime = 5;
+let strikes = 0;
 
 function startGame() {
+    strikes=0;
+    playbackTime = 5;
     fetchSongNames(); // Fetch song names when the game starts
     fetchSongsAndPlayRandom();
     createGuessInput(); // Create guess input box when the game starts
@@ -48,7 +52,20 @@ function fetchSongsAndPlayRandom() {
 function playSong(songPath) {
     const audioPlayer = document.getElementById('audioPlayer');
     audioPlayer.src = songPath;
-    audioPlayer.play();
+
+
+    audioPlayer.setAttribute('controlsList', 'nodownload');
+
+    // Add an event listener for the timeupdate event
+    audioPlayer.addEventListener('timeupdate', function(event) {
+        // Check if the current playback time exceeds 10 seconds
+        if (event.target.currentTime > playbackTime) {
+            // Pause the player and reset the playback time to 0
+            event.target.pause();
+            event.target.currentTime = 0;
+        }
+    });
+    
 }
 
 function createGuessInput() {
@@ -82,8 +99,27 @@ function updateAutocomplete(suggestions) {
 function checkGuess() {
     const userGuess = guessInput.value.trim().toLowerCase();
     const correctAnswer = currentSong.name.toLowerCase();
-    const resultMessage = userGuess === correctAnswer ? 'Congratulations! You guessed the song correctly.' : 'Sorry, incorrect guess. Try again.';
-    document.getElementById('resultText').textContent = resultMessage;
+    const resultText = document.getElementById('resultText');
+
+    if (userGuess === correctAnswer) {
+        resultText.textContent = 'Congratulations! You guessed the song correctly.';
+    } else {
+        strikes++; // Increment the number of strikes
+
+        if (strikes === 3) {    
+            // Show a popup with the game over message
+            alert('Game Over. You have reached 3 strikes.' + ' The song was : ' + currentSong.name);
+            // Restart the game
+            startGame();
+            return;
+        } else {
+            // If not the third strike, add 10 seconds to playbackTime
+            playbackTime += 5;
+            // Redisplay the result text with remaining strikes
+            resultText.textContent = 'Sorry, incorrect guess. You have ' + (3 - strikes) + ' strikes remaining.';
+        }
+    }
+    audioPlayer.play();
 }
 
 // Start the game when the page loads   
